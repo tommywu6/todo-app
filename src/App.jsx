3ce586f1624a6e0c372c77c3e16bb146a3258970
Selom <selom@kudobuzz.com>
2018-10-07 19:29:52 +0000

    imported components into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import styled from 'styled-components'
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
-const App = () => <div>Hello World</div>
+const Container = styled.div`
+    width: 250px;
+    margin: 10px auto;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 13px
+`
+
+const App = () => (
+    <Container>
+        <TodoForm />
+        <TodoList />
+    </Container>
+)
 
 render(<App />, document.getElementById('app'))
